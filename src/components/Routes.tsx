f9971a8bd6bb991bf91840ef91e83ad5baf909b3
Samuel Saputra <samuel.saputra@modalrakyat.id>
2021-01-18 00:41:40 +0700

    change Account Page to mypoke

--- src/components/Routes.tsx
+++ src/components/Routes.tsx
@@ -3,13 +3,13 @@ import { Route, Switch } from 'react-router-dom';
 
 import Home from '../containers/Home';
 import Detail from '../containers/Detail';
-import Account from '../containers/Account';
+import MyPoke from '../containers/MyPoke';
 
 const Routes = (): ReactElement => (
   <Switch>
     <Route exact path="/" component={Home} />
     <Route exact path="/pokemon/:name" component={Detail} />
-    <Route exact path="/mypoke" component={Account} />
+    <Route exact path="/mypoke" component={MyPoke} />
   </Switch>
 );
 
